e70872f10d9c678f1bb4a30c48d96597086304be
vincentbharata <vincentbharataa@gmail.com>
2025-06-25 15:45:15 +0700

    challenge day 3

---
+++ Day 3-5/angular-apps/src/app/shared/list/list.spec.ts
@@ -0,0 +1,23 @@
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { List } from './list-form';
+
+// describe('List', () => {
+//   let component: List;
+//   let fixture: ComponentFixture<List>;
+
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [List]
+//     })
+//     .compileComponents();
+
+//     fixture = TestBed.createComponent(List);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
